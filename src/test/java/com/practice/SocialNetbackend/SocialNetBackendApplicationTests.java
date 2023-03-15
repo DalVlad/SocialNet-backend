af8a29b4bab38875029fcde5984fe0270f61a5ab
Dalv <vladbulanov47@gmail.com>
2023-03-15 14:50:19 +0300

    initial commit

---
+++ src/test/java/com/practice/SocialNetbackend/SocialNetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.SocialNetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
