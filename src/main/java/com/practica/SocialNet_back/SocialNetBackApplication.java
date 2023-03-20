1016b81d3c9181ad1eed4fac30bf313b6ba7542c
Данила Ермолаев <ermolaew.danila2016@gmail.com>
2023-03-20 23:56:35 +0300

    init commit

---
+++ src/main/java/com/practica/SocialNet_back/SocialNetBackApplication.java
@@ -0,0 +1,13 @@
+package com.practica.SocialNet_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practica/SocialNet_back/SocialNetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.SocialNet_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
